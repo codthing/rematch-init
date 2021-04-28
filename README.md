63c462d87375fffe00ba0c46438513bf99d42415
fungmo <mofengfs@gmail.com>
2021-04-28 15:45:50 +0800

    基于Rematch的初始化项目，init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# rematch-init
-基于Rematch的初始化模板，轻松构建最棒的React应用
+# Rematch Example - Hooks React Typescript
+
+Example demonstrating usage of Rematch in React with Typescript and hooks.
+
+[![Edit rematch-example](https://codesandbox.io/static/img/play-codesandbox.svg)](https://codesandbox.io/s/github/rematch/rematch/tree/main/examples/hooks-react-ts?fontsize=14&hidenavigation=1&theme=dark)
---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState, Dispatch } from './store'
+
+const Count = () => {
+	const dolphins = useSelector((state: RootState) => state.dolphins)
+	const sharks = useSelector((state: RootState) => state.sharks)
+	const dispatch = useDispatch<Dispatch>()
+
+	return (
+		<div style={{ display: 'flex', flexDirection: 'row' }}>
+			<div style={{ width: 200 }}>
+				<h3>Dolphins 海豚</h3>
+				<h1>{dolphins}</h1>
+				<button onClick={() => dispatch.dolphins.increment(1)}>+1</button>
+				<button onClick={() => dispatch.dolphins.increment(3)}>+3</button>
+				<button onClick={() => dispatch.dolphins.incrementAsync(1)}>
+					Async +1
+				</button>
+			</div>
+			<div style={{ width: 200 }}>
+				<h3>Sharks 鲨鱼</h3>
+				<h1>{sharks}</h1>
+				<button onClick={() => dispatch.sharks.increment(1)}>+1</button>
+				<button onClick={() => dispatch.sharks.incrementAsync(1)}>
+					Async +1
+				</button>
+				<button
+					onClick={() =>
+						dispatch({ type: 'sharks/incrementAsync', payload: 2 })
+					}
+				>
+					Async +2
+				</button>
+			</div>
+			<p>Using Rematch Models</p>
+		</div>
+	)
+}
+
+export default Count
